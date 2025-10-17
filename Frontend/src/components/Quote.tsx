df5db79f5a750926b8044fe32399c1923d2139a4
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2025-10-17 21:19:47 +0530

    styel: adjust margin and padding

--- Frontend/src/components/Quote.tsx
+++ Frontend/src/components/Quote.tsx
@@ -12,7 +12,7 @@ export function Quote() {
               initial = "hidden"
               whileInView="visible"
               viewport={{once:true}}
-                className="mx-auto px-4 py-20 relative z-10 ">
+                className="mx-auto px-4 md:py-20 relative z-10 ">
             <Card className="max-w-4xl mx-auto bg-gradient-wisdom/20 glass-effect border-border/30 shadow-divine shadow-2xl backdrop-blur-md">
                 <CardContent className="px-12 py-4 text-center relative">
                     <div className="absolute inset-0 bg-gradient-divine opacity-5 rounded-lg " />
